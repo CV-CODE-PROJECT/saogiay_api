d30d20e0b8fe199df66a6b505dc1f153a948e938
Cuong Vo <cuongvo.2196@gmail.com>
2026-02-21 20:54:12 +0700

    Apply swagger

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,10 +1,14 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 
+@ApiTags('Hello world')
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
+  @ApiOperation({ summary: 'Hello world title' })
+  @ApiResponse({ status: 200, description: 'Success!' })
   @Get()
   getHello(): string {
     return this.appService.getHello();
--- src/main.ts
+++ src/main.ts
@@ -2,16 +2,35 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Config enviroment
   const configService = app.get(ConfigService);
   const port = configService.get<number>('app.port') ?? 3000;
-  await app.listen(port);
+  const env = configService.get<string>('app.env');
   app.useGlobalPipes(new ValidationPipe({
     whitelist: true, //remove the field don't be defined
     forbidNonWhitelisted: true, // show error if see any another field
     transform: true, // auto format type
   }));
+
+  
+  // Config swagger
+  if(env != 'production'){
+    const config = new DocumentBuilder()
+      .setTitle('NestJS Boilerplate API')
+      .setDescription('Document API for Boilerplate')
+      .setVersion('1.0')
+      .addBearerAuth() 
+      .build();
+    const document = SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup('swagger', app, document);
+  }
+
+  // run app
+  await app.listen(port);
 }
 bootstrap();
